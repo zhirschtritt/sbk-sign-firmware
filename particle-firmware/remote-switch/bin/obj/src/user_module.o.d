1734bd8d91d5a42fe22bccb581d772a4af171fbc
zhirschtirtt <zhirschtritt@gmail.com>
2018-10-08 21:04:22 -0400

    fix: remove bin dir

--- particle-firmware/remote-switch/bin/obj/src/user_module.o.d
+++
@@ -1,4 +0,0 @@
-/Users/zacharyhirschtritt/projects/sbk-led-sign/particle-firmware/remote-switch/bin//obj/src/user_module.o: \
- src/user_module.c ../../../dynalib/inc/dynalib.h
-
-../../../dynalib/inc/dynalib.h:
